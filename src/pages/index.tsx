d8aadc950f360f230372adaaa8c12bebbdf9d1eb
junaidas108 <junaidas108@gmail.com>
2024-10-02 19:06:39 +0500

    div loop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 // pages/index.tsx or a component file
 import React from 'react';
-
+import Checkout from './checkout';
 const Index = () => {
   return (
     <div>
       <iframe 
-        src="/Checkout" // URL path to the page you created
-        style={{ border: 'none', width: '100%', height: '500px' }} 
+        src="/checkout" // URL path to the page you created
+        style={{ border: 'none', width: '100%', height: '100%' }} 
         sandbox="allow-scripts allow-forms"
       >
       </iframe>
